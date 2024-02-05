43e1b68e7f68a43465bf3d2c8490ffea91015ecd
Kurumi Tsuchiya <rari.wizard@gmail.com>
2024-02-05 21:43:19 +0900

    カスタムバリデーションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
 
   def index
-    raise StandarError
     @articles = Article.all
   end
 
